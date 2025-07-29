a508bfb8e71157cb3ce8a109e0b1c6d7265f1a80
zzzzych <zzzzych@naver.com>
2025-07-29 22:45:18 +0900

    배포 오류로 인한 패키지 파일 수정

--- Package.swift
+++ Package.swift
@@ -11,22 +11,21 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
         // 🔍 Fluent ORM - 데이터베이스 연결을 위해 추가
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
-        // 🗃️ PostgreSQL 드라이버 추가
+        // 🗃️ SQLite 드라이버 (로컬용)
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        // 🗃️ PostgreSQL 드라이버 (Railway용)
         .package(url: "https://github.com/vapor/fluent-postgresql-driver.git", from: "4.0.0"),
-        // 🔐 JWT 라이브러리 - 인증 토큰 생성/검증을 위해 추가
+        // 🔐 JWT 라이브러리
         .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0")
     ],
     targets: [
         .executableTarget(
             name: "WeddingInvitationServer",
             dependencies: [
-                // Vapor 프레임워크 의존성
                 .product(name: "Vapor", package: "vapor"),
-                // Fluent ORM 의존성 - 데이터베이스 모델링
                 .product(name: "Fluent", package: "fluent"),
-                // PostgreSQL 드라이버 의존성 - PostgreSQL 연결
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "FluentPostgreSQLDriver", package: "fluent-postgresql-driver"),
-                // JWT 의존성 - 토큰 인증
                 .product(name: "JWT", package: "jwt")
             ]
         ),
