5ff9aafca88deafafed9efc1d6818e6e85ebe513
zzzzych <zzzzych@naver.com>
2025-07-29 22:14:36 +0900

    오류 확인을 위한 커밋

--- Package.swift
+++ Package.swift
@@ -12,7 +12,9 @@ let package = Package(
         // 🔍 Fluent ORM - 데이터베이스 연결을 위해 추가
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
         // 🗃️ SQLite 드라이버 - SQLite 데이터베이스 사용을 위해 추가
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        // .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        // PostgreSQL
+        .product(name: "FluentPostgreSQLDriver", package: "fluent-postgresql-driver"),
         // 🔐 JWT 라이브러리 - 인증 토큰 생성/검증을 위해 추가
         .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0")
     ],
@@ -25,7 +27,9 @@ let package = Package(
                 // Fluent ORM 의존성 - 데이터베이스 모델링
                 .product(name: "Fluent", package: "fluent"),
                 // SQLite 드라이버 의존성 - SQLite 연결
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                // .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                // PostgreSQL
+                .package(url: "https://github.com/vapor/fluent-postgresql-driver.git", from: "4.0.0"),
                 // JWT 의존성 - 토큰 인증
                 .product(name: "JWT", package: "jwt")
             ]
